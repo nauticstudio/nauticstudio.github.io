c426c9f410e857278b76eec89f90555f30e2aa7e
nauticstudio <nauticboyofficial@gmail.com>
2025-02-13 15:52:41 -0300

    js update COMMIT

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,20 @@
         window.addEventListener("resize", checkVisibility);
         checkVisibility(); // Verificar al cargar la página
     });
+
+  // Intenta cargar Bootstrap desde el CDN
+  function loadBootstrap() {
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js";
+    script.integrity = "sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz";
+    script.crossOrigin = "anonymous";
+    script.onerror = () => {
+      // Si el CDN falla, carga la versión local
+      const fallbackScript = document.createElement("script");
+      fallbackScript.src = "./assets/js/bootstrap.bundle.min.js";
+      document.body.appendChild(fallbackScript);
+    };
+    document.body.appendChild(script);
+  }
+
+  loadBootstrap();
