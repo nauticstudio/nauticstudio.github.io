968351577ba0e05c814f2a1a21278517af492d0c
nauticstudio <nauticboyofficial@gmail.com>
2025-02-13 15:44:13 -0300

    Website Update COMMIT

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
-AOS.init();
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 700, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    document.addEventListener("DOMContentLoaded", () => {
+        const animateElements = document.querySelectorAll(".nautic-animate");
 
-});
+        const checkVisibility = () => {
+            animateElements.forEach((element) => {
+                const rect = element.getBoundingClientRect();
+                const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+
+                if (isVisible) {
+                    const delay = element.getAttribute("data-animate-delay") || 0;
+                    setTimeout(() => {
+                        element.classList.add("animate");
+                    }, delay);
+                }
+            });
+        };
+
+        window.addEventListener("scroll", checkVisibility);
+        window.addEventListener("resize", checkVisibility);
+        checkVisibility(); // Verificar al cargar la página
+    });
